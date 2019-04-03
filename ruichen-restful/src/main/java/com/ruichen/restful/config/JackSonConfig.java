3994b5a6972e46b121da762eb5b63031d71326f3
lixueyun <lixy16@ziroom.com>
2019-04-03 17:52:10 +0800

    增加数据源、redis配置待完善

---
+++ ruichen-restful/src/main/java/com/ruichen/restful/config/JackSonConfig.java
@@ -0,0 +1,32 @@
+package com.ruichen.restful.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ClassName  JackSonConfig
+ * @Description jackSon配置类
+ * @author  lixueyun
+ * @Date  2019/4/3 17:44
+ */
+@Configuration
+public class JackSonConfig {
+    @Bean
+    public ObjectMapper objectMapper(){
+        ObjectMapper objectMapper = new ObjectMapper()
+                .registerModule(new ParameterNamesModule())
+                .registerModule(new Jdk8Module())
+                .registerModule(new JavaTimeModule());
+        // 忽略json字符串中不识别的属性
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        // 忽略无法转换的对象 “No serializer found for class com.xxx.xxx”
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS,false);
+        return objectMapper;
+    }
+}
